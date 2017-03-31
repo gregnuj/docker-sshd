6933e7cbcb725b5f0d32a332f4039a1f74e44580
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-03-31 23:48:35 +0000

    added more functions to *_info scripts

--- rootfs/usr/local/bin/docker_info.sh
+++ rootfs/usr/local/bin/docker_info.sh
@@ -5,22 +5,12 @@ if [[ ! -z "$DEBUG" && "$DEBUG" != 0 && "${DEBUG^^}" != "FALSE" ]]; then
   set -x
 fi
 
-declare FQDN="${FQDN:=""}"
-declare NAME_SERVER="${NAME_SERVER:=""}"
-declare SERVICE_NAME="${SERVICE_NAME:=""}"
-declare SERVICE_HOSTNAME="${SERVICE_HOSTNAME:=""}"
-declare SERVICE_INSTANCE="${SERVICE_INSTANCE:=""}"
-declare SERVICE_MEMBERS="${SERVICE_MEMBERS:=""}"
-declare SERVICE_MINIMUM="${SERVICE_MINIMUM:="2"}"
-declare CONTAINER_NAME="${CONTAINER_NAME:=""}"
-declare NODE_ADDRESS="${NODE_ADDRESS:=""}"
-
-function name_server(){
-    if [[ -z "$NAME_SERVER" ]] ; then
-        NAME_SERVER="$(awk '/nameserver/{print $2}' /etc/resolv.conf | tail -n1)"
-    fi
-    echo "$NAME_SERVER"
-}
+declare FQDN="$(fqdn)"
+declare NODE_ADDRESS="$(node_address)"
+declare SERVICE_NAME="$(service_name)"
+declare SERVICE_HOSTNAME="$(service_hostname)"
+declare SERVICE_INSTANCE="$(service_instance)"
+declare CONTAINER_NAME="$(container_name)"
 
 function node_address(){
     while [[ -z "$NODE_ADDRESS" ]] ; do
@@ -36,7 +26,7 @@ function node_address(){
 
 function fqdn(){
     while [[ -z "$FQDN" && $LOOP -lt 30 ]] ; do
-        FQDN="$(nslookup "$(node_address)" "$(name_server)" | awk -F'= ' 'NR==5 { print $2 }')"
+        FQDN="$(nslookup "$(node_address)" | awk -F'= ' 'NR==5 { print $2 }')"
         SERVICE_NAME="$(echo "$FQDN" | awk -F'.' '{print $1}')"
         SERVICE_LOOKUP="$(getent hosts tasks.${SERVICE_NAME})"
         if [[ -z "${SERVICE_LOOKUP}" ]]; then
@@ -47,23 +37,17 @@ function fqdn(){
 }
 
 function service_hostname(){
-    if [[ -z "$SERVICE_HOSTNAME" ]] ; then
-        SERVICE_HOSTNAME="$(echo "$(fqdn)" | awk -F'.' '{print $1 "." $2}')"
-    fi
+    SERVICE_HOSTNAME="${SERVICE_NAME:="$(echo "$(fqdn)" | awk -F'.' '{print $1 "." $2}')"}"
     echo "$SERVICE_HOSTNAME"
 }
 
 function service_name(){
-    if [[ -z $SERVICE_NAME ]] ; then
-        SERVICE_NAME="$(echo "$(fqdn)" | awk -F'.' '{print $1}')"
-    fi
+    SERVICE_NAME="${SERVICE_NAME:="$(echo "$(fqdn)" | awk -F'.' '{print $1}')"}"
     echo "$SERVICE_NAME"
 }
 
 function service_instance(){
-    if [[ -z $SERVICE_INSTANCE ]] ; then
-        SERVICE_INSTANCE="$(echo "$(fqdn)" | awk -F'.' '{print $2}')"
-    fi
+    SERVICE_INSTANCE="${SERVICE_INSTANCE:="$(echo "$(fqdn)" | awk -F'.' '{print $2}')"}"
     echo "$SERVICE_INSTANCE"
 }
 
@@ -76,35 +60,13 @@ function container_name(){
 }
 
 function service_members(){
-    while [[ -z "$SERVICE_MEMBERS" ]]; do
-       SERVICE_NAME="$(service_name)"
-       NODE_ADDRESS="$(node_address)"
-       CURRENT_MEMBERS="$(getent hosts tasks.${SERVICE_NAME} | sort | awk -v ORS=',' '{print $1}')"
-       CURRENT_MEMBERS="${CURRENT_MEMBERS%%,}" # strip trailing commas
-       COUNT=$(echo "$CURRENT_MEMBERS" | tr ',' ' ' | wc -w)
-       echo "Found ($COUNT) members in ${SERVICE_NAME} ($CURRENT_MEMBERS)" >&2
-       if [[ $COUNT -lt $(($SERVICE_MINIMUM)) ]]; then
-           echo "Waiting for at least $SERVICE_MINIMUM IP addresses to resolve..." >&2
-           SLEEPS=$((SLEEPS + 1))
-           sleep 3
-       else
-           SERVICE_MEMBERS="$CURRENT_MEMBERS"
-       fi
-
-       # After 90 seconds reduce SERVICE_ADDRESS_MINIMUM
-       if [[ $SLEEPS -ge 30 ]]; then
-          SLEEPS=0
-          SERVICE_MINIMUM=$((SERVICE_MINIMUM - 1))
-          echo "Reducing SERVICE_MINIMUM to $SERVICE_MINIMUM" >&2
-       fi
-       if [[ $SERVICE_MINIMUM -lt 2 ]]; then
-          echo "SERVICE_MINIMUM is $SERVICE_MINIMUM cannot continue" >&2
-          exit 1
-       fi
-    done
-    echo $SERVICE_MEMBERS
+    CURRENT_MEMBERS="$(getent hosts tasks.$(service_name) | sort | awk -v ORS=',' '{print $1}')"
+    echo "${CURRENT_MEMBERS%%,}" # strip trailing commas
 }
 
+function service_count(){
+    COUNT=$(echo "$(service_members)" | tr ',' ' ' | wc -w)
+}
 
 function main(){
     case "$1" in
