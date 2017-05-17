15e1ea355a6aaee0c8b3b269bbc0d8760ae8b426
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-05-17 19:19:48 +0000

    modified useradd

--- rootfs/etc/entrypoint.d/add-users.sh
+++ rootfs/etc/entrypoint.d/add-users.sh
@@ -4,7 +4,7 @@ for USER in ${APP_USERS}; do
     if [[ ! -z "${USER}" ]]; then
         if [[ -d "/home/${USER}" ]]; then
             ls -la /home | grep "${USER}" |
-            awk "{print \"useradd\", \"-u\", \$3, \"-g\", \$4, \"-s\", \"/bin/bash\", \"${USER}\"}" |
+            awk "{print \"useradd\", \"-u\", \$3, \"-G\", \$4, \"-s\", \"/bin/bash\", \"${USER}\"}" |
 	    while read command; do $command; done
         fi
     fi
